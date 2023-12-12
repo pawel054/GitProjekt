368a2ca69f7ead45c00755ec4119636ea5d0e03a
pawel054 <pawel.marcisz1@gmail.com>
2023-12-12 12:52:00 +0100

    added window

--- GitProjekt/GitProjekt/App.xaml.cs
+++ GitProjekt/GitProjekt/App.xaml.cs
@@ -10,7 +10,7 @@ namespace GitProjekt
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
