f081239f1ccf3b8eec005dd38f91819b931c2eb0
pawel054 <pawel.marcisz1@gmail.com>
2023-12-12 14:19:42 +0100

    updated ManageProduct

--- GitProjekt/GitProjekt/App.xaml.cs
+++ GitProjekt/GitProjekt/App.xaml.cs
@@ -10,7 +10,7 @@ namespace GitProjekt
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new ManageProduct();
         }
 
         protected override void OnStart()
--- GitProjekt/GitProjekt/ManageProduct.xaml.cs
+++ GitProjekt/GitProjekt/ManageProduct.xaml.cs
@@ -14,7 +14,48 @@ namespace GitProjekt
 	{
 		public ManageProduct ()
 		{
-			InitializeComponent ();
+			InitializeComponent();
 		}
-	}
+
+        public ManageProduct(string test)
+        {
+            InitializeComponent();
+			labelTytul.Text = "Edytuj produkt";
+			btnDodaj.IsVisible = false;
+			btnEdytuj.IsVisible = true;
+        }
+
+		private void BtnDodajClicked(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrEmpty(entryNazwa.Text) && !string.IsNullOrEmpty(entryCena.Text) && !string.IsNullOrEmpty(entryIlosc.Text))
+			{
+                if (int.TryParse(entryIlosc.Text, out int _) && decimal.TryParse(entryCena.Text, out decimal _))
+                {
+                    
+                }
+                else
+                {
+                    DisplayAlert("Niepoprawne dane", "Pole cena lub ilość nie jest liczbą", "OK");
+                }
+            }
+			else
+			{
+				DisplayAlert("Niepoprawne dane", "Pola nie mogą byc puste", "OK");
+			}
+
+		}
+
+        private void BtnEdytujClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(entryNazwa.Text) && !string.IsNullOrEmpty(entryCena.Text) && !string.IsNullOrEmpty(entryIlosc.Text))
+            {
+
+            }
+            else
+            {
+                DisplayAlert("Niepoprawne dane", "Pola nie mogą byc puste", "OK");
+            }
+
+        }
+    }
 }
